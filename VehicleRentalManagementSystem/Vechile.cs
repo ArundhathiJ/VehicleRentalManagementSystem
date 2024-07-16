d15f24037195351b0b6cee613413cb1643c5a65c
Arundhathi Jayaprakash <Ajayaprakash6095@conestogac.on.ca>
2024-07-16 18:16:45 -0400

    Add project files.

---
+++ VehicleRentalManagementSystem/Vechile.cs
@@ -0,0 +1,12 @@
+﻿public class Vehicle
+{
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+    public decimal RentalPrice { get; set; }
+
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}, Rental Price: {RentalPrice:C}");
+    }
+}
